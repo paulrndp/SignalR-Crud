5f7b509a063c0f274aee3ec1198afee273aaf439
paulrndp <paulr@DESKTOP-47REJSO>
2022-09-01 20:04:28 +0800

    add Product model class

---
+++ SignalRCrud/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SignalRCrud.Models
+{
+    public class Products
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Item { get; set; }
+        public string? Category { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+    }
+}
